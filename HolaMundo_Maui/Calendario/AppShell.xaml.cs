2d252b9bff41decb1d2e3e6f1bf95c9b20d251d9
fjmiguel <fernandoj.miguel@iesnervion.es>
2023-10-13 08:49:13 +0200

    Hecho

---
+++ HolaMundo_Maui/Calendario/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Calendario
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HolaMundo_Maui/Calendario/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace Calendario
+{
+    public partial class MainPage : ContentPage
+    {
+        public DateTime FechaHoy { get; } = DateTime.Now.Date;
+        public DateTime FechaEntrada { get; set; }
+        public DateTime FechaSalida { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+
+            entrada.DateSelected += (sender, e) =>
+            {
+                FechaEntrada = e.NewDate.Date;
+                salida.MinimumDate = FechaEntrada;
+            };
+
+            salida.DateSelected += (sender, e) =>
+            {
+                FechaSalida = e.NewDate.Date;
+            };
+
+        }
+
+
+    }
+}
